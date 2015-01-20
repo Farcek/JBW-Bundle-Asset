6caa3494dda8405eb7e336727b5753c3a0d49c4c
farcek <Farcek@gmail.com>
2015-01-20 14:48:58 +0800

    init

---
+++ src/main/java/mn/le/farcek/jbw/bundle/asset/JBWBundleAsset.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mn.le.farcek.jbw.bundle.asset;
+
+
+import java.util.List;
+
+import mn.le.farcek.jbw.api.Bundle;
+
+import mn.le.farcek.jbw.api.bundle.JBWBundle;
+
+
+
+/**
+ *
+ * @author Farcek
+ */
+@Bundle(name = "asset")
+public class JBWBundleAsset extends JBWBundle {
+
+    @Override
+    protected void setupControllers(List<Class<?>> controllers) {
+        
+    }
+
+   
+}
